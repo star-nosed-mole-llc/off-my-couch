3a40d782a2f04b6aeff572c1c73366335f351924
Vanaldito <fredimanuelb@gmail.com>
2022-04-01 12:55:54 -0600

    Home page improved

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const app = express();
 const assetsRouter = require("./server/assets-router");
 
 app.use("/", express.static(path.join(__dirname, "public")));
-app.use("/", assetsRouter)
+app.use("/", assetsRouter);
 
 app.get("/api/v1", (req, res) => {
   res.json({
-    name: "Fredi",
-    lastName: "Barraza",
+    project: "React and Express Boilerplate",
+    from: "Vanaldito",
   });
-})
+});
 
 app.listen(5000, () => {
   console.log("Server running in port 5000");
--- server/assets-router.js
+++ server/assets-router.js
@@ -6,7 +6,7 @@ const imageRegex = /\/.+\.(svg|png|jpg|png|jpeg)$/;
 
 router.get(imageRegex, (req, res) => {
   const filePath = req.path;
-  res.redirect(303, `http://localhost:3000${filePath}`)
+  res.redirect(303, `http://localhost:3000${filePath}`);
 });
 
 module.exports = router;
