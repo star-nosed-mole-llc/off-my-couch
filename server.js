c8bce8fe17fb4dccb051f7fc5a39b58183c21fac
Vanaldito <fredimanuelb@gmail.com>
2022-04-04 22:03:35 -0600

    Problem with middleware in / solved

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const assetsRouter = require("./server/assets-router");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(assetsRouter);
 
 app.get("/api/v1", (req, res) => {
@@ -16,7 +16,7 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.get("/*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 })
 
 const { PORT = 5000 } = process.env;
